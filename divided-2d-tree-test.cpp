0c31a2681e0bc8bebf8b1224531aab11a97567b9
mathsjames <james.bell1@hotmail.com>
2017-08-15 17:20:56 +0100

    added skeleton for 3 functions and skeleton for tests of functions

--- divided-2d-tree-test.cpp
+++ divided-2d-tree-test.cpp
@@ -1,5 +1,35 @@
 #include"divided-2d-tree.h"
 
+int testInsert() {
+  return 0;
+}
+
+int testRemove() {
+  return 0;
+}
+
+int testDeleteTree() {
+  return 0;
+}
+
+int testNearestK() {
+  return 0;
+}
+
+int testDistanceToNearestK() {
+  return 0;
+}
+
+int testNearestAndNextDistance() {
+  return 0;
+}
+
 int main() {
+  int failures = 0;
+  failures+=testInsert();
+  failures+=testRemove();
+  failures+=testNearestK();
+  failures+=testDistanceToNearestK();
+  failures+=testNearestAndNextDistance();
   return 0;
 }
--- divided-2d-tree.h
+++ divided-2d-tree.h
@@ -2,22 +2,30 @@ struct DataPoint {
   double x,y;
 };
 
-struct kPoints {
+struct KPoints {
   int k;
   DataPoint *points;
 };
 
-struct kDistances {
+struct KDistances {
   int k;
   double *dists;
 };
 
+struct PointAnd2Distances {
+  DataPoint point;
+  double dist1;
+  double dist2;
+};
 
 class D2dTreeNode {
   DataPoint point;
   D2dTreeNode *left,*right;
 public:
   void insert(DataPoint newpoint);
-  kPoints nearestK(DataPoint newpoint,int k, double maxdist);
-  kDistances distanceToNearestK(DataPoint newpoint, int k, double maxdist);
+  void remove(DataPoint oldpoint);
+  void deleteTree();
+  KPoints nearestK(DataPoint otherpoint,int k, double maxdist);
+  KDistances distanceToNearestK(DataPoint otherpoint, int k, double maxdist);
+  PointAnd2Distances nearestAndNextDistance(DataPoint otherpoint, double maxdist);
 };
